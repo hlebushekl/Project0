9925b23cb4a8b26d03a052083a68e7d608f702c5
Arefev <127677998+hlebushekl@users.noreply.github.com>
2024-03-20 17:49:52 +0300

    Add files via upload

---
+++ Sotov/MainActiv.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sotov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainActiv.xaml
+    /// </summary>
+    public partial class MainActiv : Window
+    {
+        public MainActiv()
+        {
+            InitializeComponent();
+        }
+
+        private void Kabinet_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow kl = new MainWindow();
+            kl.Show();
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+    }
+}
